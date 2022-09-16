8ef964274a6aa921273bed235562ccaddaff0b05
JohnAlfredHebres <johnalfredhebres20@gmail.com>
2022-09-16 12:56:57 -1100

    update

---
+++ phpEx5.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>phpEx5</title>
+</head>
+<body>
+
+
+<?php   
+ $around = "around"; 
+echo 'What goes ' . $around . ' comes ' . $around . '.';
+?> 
+	
+	
+</body>
+</html>
